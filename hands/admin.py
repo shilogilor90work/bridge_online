180134a0afec7aaa39f034a5c15fbcb4ccff9d94
shilo gilor <shilo1>
2024-12-25 10:26:59 +0200

    feat: initialize the django

---
+++ hands/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Hand
+
+admin.site.register(Hand)
---
+++ hands/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Hand(models.Model):
+    subject = models.CharField(max_length=100)
+    created_at = models.DateTimeField('Created', auto_now_add=True)
+    update_at = models.DateTimeField('Updated', auto_now=True)
+    cards = models.CharField(max_length=200)
+    bids = models.CharField(max_length=300)
+    correct_answer = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
