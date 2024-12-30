f16927fa8950a4afb0b82afb40172d694379b336
shilo gilor <shilo1>
2024-12-30 12:42:19 +0200

    feat: no shuffle

--- manage/urls.py
+++ manage/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('<int:hand_id>/update/', views.update_hand, name='update_hand'),
     path('<int:hand_id>/delete/', views.delete_hand, name='delete_hand'),
     path('hand_list/', views.hand_list, name='hand_list'),
+    path('hand_list_no_shuffle/', views.hand_list_no_shuffle, name='hand_list'),
     path('update-answer/<int:hand_id>/<str:answer>/<str:explanation>/', views.update_answer, name='update_answer'),
     path('upload-json/', views.upload_json_view, name='upload_json'),
 ]
