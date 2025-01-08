478b0300cb0a0cd2b88c0aca5e7f8bfee4ca490a
shilo gilor <shilo1>
2025-01-08 11:25:39 +0200

    feat: update length to db char

--- hands/models.py
+++ hands/models.py
@@ -5,13 +5,13 @@ class Hand(models.Model):
     subject = models.CharField(max_length=100)
     created_at = models.DateTimeField('Created', auto_now_add=True)
     update_at = models.DateTimeField('Updated', auto_now=True)
-    cards = models.CharField(max_length=200)
-    bids = models.CharField(max_length=300)
+    cards = models.CharField(max_length=500)
+    bids = models.CharField(max_length=1000)
     correct_answer = models.CharField(max_length=300)
     user_updated = models.CharField(max_length=100, blank=True, null=True)
     metadata = models.JSONField(blank=True, null=True)  # For JSON data
     optional_bids = models.CharField(max_length=300, blank=True, null=True)
-    explanation = models.CharField(max_length=300, blank=True, null=True)
+    explanation = models.CharField(max_length=1000, blank=True, null=True)
     ns_vul = models.BooleanField(default=False)
     ew_vul = models.BooleanField(default=False)
 
